eb7796782eb3060a04eeaa7ed3e3e2bcd972a05c
123Ryan321 <ryan.3@live.com.au>
2015-09-21 18:13:39 +1000

    first commit

---
+++ js/shared.js
@@ -0,0 +1,13 @@
+var Header = new function() {
+
+
+	this.header = document.getElementById("header");
+
+	this.header.innerHTML = '<ul>' +
+			'<l><a href="about.html">About</a></l> ' +
+			'<l><a href="project.html">Project</a></l> ' +
+			'<l><a href="portfolio.html">Portfolio</a></l> '+
+			'<l><a href="reflection.html">Reflection</a></l> '+
+		'</ul>';
+
+}
